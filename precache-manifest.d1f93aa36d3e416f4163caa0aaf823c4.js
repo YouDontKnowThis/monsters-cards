61e2cdf1e2e6bb0cb6c780ff08ac33c319f0e66c
bk dev <fox.physics@outlook.fr>
2020-08-23 03:02:53 +0100

    Updates

---
+++ precache-manifest.d1f93aa36d3e416f4163caa0aaf823c4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3145166dace8d1ab355785ebf17169b8",
+    "url": "/monsters-cards/index.html"
+  },
+  {
+    "revision": "9c3a7e6dced583e4023b",
+    "url": "/monsters-cards/static/css/main.2d1ecdd8.chunk.css"
+  },
+  {
+    "revision": "17a9ff1603a390c18467",
+    "url": "/monsters-cards/static/js/2.ba594e8a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-cards/static/js/2.ba594e8a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9c3a7e6dced583e4023b",
+    "url": "/monsters-cards/static/js/main.38028101.chunk.js"
+  },
+  {
+    "revision": "b6c72e5a2f55d7b3e4a8",
+    "url": "/monsters-cards/static/js/runtime-main.b257fb17.js"
+  }
+]);
